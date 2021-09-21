24bbd8cfe706ad9b849cbd4cacf6c77589293f76
MomenAmiin <Momenamiin@gmail.com>
2021-09-21 11:52:29 +0200

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,52 +11,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 30,
-              child: Image.asset('assets/Images/view.png',),
-            ),
-            Expanded(child: SizedBox()),
-            Text(
-              'Accelerated Learning',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w300,
-                  fontFamily: "Oswald"),
-            ),
-            Expanded(child: SizedBox()),
-          ],
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            color: Colors.black,
-            tooltip: "search",
-            onPressed: () {},
-          ),
-          PopupMenuButton<String>(
-            icon: Icon(
-              Icons.more_vert,
-              color: Colors.black,
-            ),
-            onSelected: (value) => value == "info",
-            itemBuilder: (BuildContext context) {
-              return {'info'}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
-          ),
-        ],
-      ),
-    );
+appBar: AppBar(
+title: Text('Quiz App'),
+    ),
+  );
   }
 }
