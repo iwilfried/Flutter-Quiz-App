030e84e09486bf733401925cd2c9697eea469e85
MomenAmiin <Momenamiin@gmail.com>
2021-10-12 15:01:42 +0200

    adding upper border to appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,52 +10,59 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 30,
-              child: Image.asset('assets/images/LogoMaster.png'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          shape: Border(top: BorderSide(color: Colors.green, width: 3)),
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          titleSpacing: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 30,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Image.asset('assets/images/LogoMaster.png'),
+              ),
+              Expanded(child: SizedBox()),
+              FittedBox(
+                child: Text(
+                  'Accelerated Learning',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: "Roboto"),
+                ),
+              ),
+              Expanded(child: SizedBox()),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              color: Colors.black,
+              tooltip: "search",
+              onPressed: () {},
             ),
-            Expanded(child: SizedBox()),
-            Text(
-              'Accelerated Learning',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w300,
-                  fontFamily: "Roboto"),
+            PopupMenuButton<String>(
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.black,
+              ),
+              onSelected: (value) => value == "info",
+              itemBuilder: (BuildContext context) {
+                return {'info'}.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
             ),
-            Expanded(child: SizedBox()),
           ],
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            color: Colors.black,
-            tooltip: "search",
-            onPressed: () {},
-          ),
-          PopupMenuButton<String>(
-            icon: Icon(
-              Icons.more_vert,
-              color: Colors.black,
-            ),
-            onSelected: (value) => value == "info",
-            itemBuilder: (BuildContext context) {
-              return {'info'}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
-          ),
-        ],
       ),
     );
   }
